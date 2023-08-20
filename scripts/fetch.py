7fe7204fc5373bb13b5282c0f7446c9327131913
Alan-Kuan <kuanhaoan@gmail.com>
2023-08-21 00:28:06 +0800

    refactor: reorganize data

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -17,13 +17,17 @@ data = res.json()
 with open(history_path) as f:
     history = json.load(f)
 
+mapping = {
+    '112-1研究所宿舍申請': 'graduate',
+    '112-1學士班學生申請校內宿舍(舊生候補)': 'undergraduate',
+    '學士班新生宿舍暑假住宿申請作業': 'summer',
+}
+
 for item in data:
-    new_item = {
-        'date': str(date.today()),
-        'male': item['male'],
-        'female': item['female']
-    }
-    history[item['name']].append(new_item)
+    key = mapping[item['name']]
+    history[key]['date'].append(str(date.today()))
+    history[key]['male'].append(item['male'])
+    history[key]['female'].append(item['female'])
 
 with open(history_path, 'w', encoding='utf8') as f:
     json.dump(history, f, ensure_ascii=False, indent=4)
